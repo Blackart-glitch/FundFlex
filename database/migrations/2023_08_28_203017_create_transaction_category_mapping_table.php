aee05964544a44ee65e2f4fb66a10bcfccbd6198
Daniel Boye [Author] <boyelanrewaju@gmail.com>
2023-08-28 21:39:27 +0100

    migrations for DB schema

---
+++ database/migrations/2023_08_28_203017_create_transaction_category_mapping_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('transaction_category_mapping', function (Blueprint $table) {
+            $table->unsignedBigInteger('transaction_id');
+            $table->unsignedBigInteger('category_id');
+
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('transaction_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_category_mapping');
+    }
+};
