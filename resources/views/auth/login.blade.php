4f608bf098ec78b88282f7c0c1909be55bb48ff7
Daniel Boye [Author] <boyelanrewaju@gmail.com>
2023-08-29 04:48:23 +0100

    basic template for dashboard/login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,56 +10,57 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
+
 <body class="bg-light">
 
-<div class="min-vh-100 d-flex align-items-center justify-content-center">
-    <div class="max-w-md w-100 bg-white p-4 rounded shadow">
-        <!-- Session Status -->
-        @if (session('status'))
-            <div class="alert alert-success mb-4">{{ session('status') }}</div>
-        @endif
+    <div class="d-flex min-vh-100 flex-column justify-content-center px-4 py-5 lg:px-5">
+        <div class="mx-auto w-full max-w-sm">
+            <img class="mx-auto mb-4" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company" height="40">
+            <h2 class="mt-6 text-center text-2xl font-bold leading-9 text-gray-900">{{ __('Sign in to your Account') }}
+            </h2>
+        </div>
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        <div class="mt-8 mx-auto w-full max-w-sm">
+            <form class="space-y-4" action="{{ route('login') }}" method="POST">
+                @csrf
+                <div>
+                    <label for="email"
+                        class="block text-sm font-medium text-gray-900">{{ __('Email Address') }}</label>
+                    <div class="mt-1">
+                        <input id="email" name="email" type="email" autocomplete="email" required
+                            class="form-control py-2.5 text-gray-900 shadow-sm focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+                        @error('email')
+                            <p class="text-red-500 text-sm">{{ $message }}</p>
+                        @enderror
+                    </div>
+                </div>
 
-            <!-- Email Address -->
-            <div class="mb-3">
-                <label for="email" class="form-label">{{ __('Email') }}</label>
-                <input id="email" class="form-control" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username">
-                @error('email')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </div>
+                <div class="d-flex justify-content-between align-items-center">
+                    <label for="password" class="block text-sm font-medium text-gray-900">{{ __('Password') }}</label>
+                    <div class="text-sm">
+                        <a href="#"
+                            class="font-weight-semibold text-indigo-600 hover:text-indigo-500">{{ __('Forgot Password?') }}</a>
+                    </div>
+                </div>
+                <div class="mt-1">
+                    <input id="password" name="password" type="password" autocomplete="current-password" required
+                        class="form-control py-2.5 text-gray-900 shadow-sm focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+                    @error('password')
+                        <p class="text-red-500 text-sm">{{ $message }}</p>
+                    @enderror
+                </div>
 
-            <!-- Password -->
-            <div class="mb-3">
-                <label for="password" class="form-label">{{ __('Password') }}</label>
-                <input id="password" class="form-control" type="password" name="password" required autocomplete="current-password">
-                @error('password')
-                    <p class="text-danger">{{ $message }}</p>
-                @enderror
-            </div>
-
-            <!-- Remember Me -->
-            <div class="form-check mb-3">
-                <input id="remember_me" class="form-check-input" type="checkbox" name="remember">
-                <label for="remember_me" class="form-check-label">{{ __('Remember me') }}</label>
-            </div>
-
-            <div class="d-grid">
-                @if (Route::has('password.request'))
-                    <a class="text-muted text-sm mb-2" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
-                <button type="submit" class="btn btn-primary">{{ __('Log in') }}</button>
-            </div>
-        </form>
+                <div>
+                    <button type="submit"
+                        class="btn btn-indigo btn-block py-2.5 font-semibold">{{ __('Sign in') }}</button>
+                </div>
+            </form>
+        </div>
     </div>
-</div>
 
-<!-- Bootstrap JS (Optional) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Bootstrap JS (Optional) -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Bootstrap CSS -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fund Wallet App Dashboard</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
 <body class="bg-light">
 
     <div class="min-vh-100 d-flex align-items-center justify-content-center">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white p-6 rounded shadow-sm">
+        <div class="container">
+            <div class="bg-white p-4 rounded shadow-sm">
                 <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
                     {{ __('Dashboard') }}
                 </h2>
@@ -26,8 +23,130 @@
         </div>
     </div>
 
-    <!-- Bootstrap JS (Optional) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <div class="container-fluid">
+        <div class="row">
+            <!-- Sidebar -->
+            <div class="col-md-3 bg-dark text-white py-4">
+                <a href="/"
+                    class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <svg class="bi pe-none me-2" width="40" height="32">
+                        <use xlink:href="#bootstrap"></use>
+                    </svg>
+                    <span class="fs-4">Sidebar</span>
+                </a>
+                <hr>
+                <ul class="nav nav-pills flex-column mb-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active" aria-current="page">
+                            <svg class="bi pe-none me-2" width="16" height="16">
+                                <use xlink:href="#home"></use>
+                            </svg>
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link text-white">
+                            <svg class="bi pe-none me-2" width="16" height="16">
+                                <use xlink:href="#speedometer2"></use>
+                            </svg>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link text-white">
+                            <svg class="bi pe-none me-2" width="16" height="16">
+                                <use xlink:href="#table"></use>
+                            </svg>
+                            Settings
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link text-white">
+                            <svg class="bi pe-none me-2" width="16" height="16">
+                                <use xlink:href="#grid"></use>
+                            </svg>
+                            History
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="nav-link text-white">
+                            <svg class="bi pe-none me-2" width="16" height="16">
+                                <use xlink:href="#people-circle"></use>
+                            </svg>
+                            Profile
+                        </a>
+                    </li>
+                </ul>
+                <hr>
+                <div class="dropdown">
+                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://github.com/mdo.png" alt="" width="32" height="32"
+                            class="rounded-circle me-2">
+                        <strong>mdo</strong>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
+                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <!-- Main content -->
+            <main class="col-md-9 ml-md-auto px-4">
+                <!-- Transaction History -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- Transaction history table -->
+                    </div>
+                </div>
+
+                <!-- Account Summary and Graph -->
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Account Summary</h2>
+                        <p><strong>Account Balance:</strong> $1,200</p>
+                        <p><strong>Available Funds:</strong> $800</p>
+                        <p><strong>Pending Transactions:</strong> 2</p>
+                    </div>
+                    <div class="col-md-6">
+                        <h2>Graphs and Charts</h2>
+                        <div class="text-center">
+                            <h4>Placeholder Graph</h4>
+                            <p>This is where the actual graph will go.</p>
+                        </div>
+                        <div class="d-flex align-items-end">
+                            <div class="flex-shrink-0">
+                                <img src="#" alt="" width="300">
+                            </div>
+                            <div class="flex-grow-1 ms-3">
+                                <h5 class="mt-0">Media heading</h5>
+                                <p>This is some content from a media component</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Quick Actions -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Quick Actions</h2>
+                        <a href="#" class="btn btn-primary">Send Money</a>
+                        <a href="#" class="btn btn-success">Receive Money</a>
+                        <a href="#" class="btn btn-info">Request Funds</a>
+                        <a href="#" class="btn btn-secondary">Make Payment</a>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 
 </html>
