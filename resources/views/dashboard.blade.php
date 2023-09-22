f73ca80524b45a1a64128917de392ae0723c9f0f
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-22 17:19:19 +0100

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,148 +5,146 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fund Wallet App Dashboard</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
 </head>
 
-<body class="bg-light">
+<body class="bg-gray-100 font-sans">
 
-    <div class="min-vh-100 d-flex align-items-center justify-content-center">
-        <div class="container">
-            <div class="bg-white p-4 rounded shadow-sm">
-                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-4">
-                    {{ __('Dashboard') }}
-                </h2>
-                <div class="text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="container-fluid">
-        <div class="row">
-            <!-- Sidebar -->
-            <div class="col-md-3 bg-dark text-white py-4">
-                <a href="/"
-                    class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <svg class="bi pe-none me-2" width="40" height="32">
-                        <use xlink:href="#bootstrap"></use>
+    <div class="flex h-screen bg-gray-200">
+        <!-- Sidebar -->
+        <div class="flex-shrink-0 w-56 bg-white border-r">
+            <div class="">
+                <img src="{{ asset('logo.png') }}" alt="logo" class="w-40 h-36 mx-auto">
+                <!-- <div class="text-xl font-semibold text-gray-800">Babatunde</div>
+                <a href="#" class="flex items-center mt-2 text-gray-600 hover:text-indigo-600">
+                    <span class="mr-2">
+                        Profile
+                    </span>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+                        <path fill-rule="evenodd" d="M5.293 9.293a1 1 0 011.414 0L10 12.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                     </svg>
-                    <span class="fs-4">Sidebar</span>
+                </a> -->
+            </div>
+            <nav class="mt-6">
+                <a href="/dashboard" class="flex items-center px-4 py-2 text-gray-600 hover:text-indigo-600 hover:bg-gray-100">
+                    <img src="{{ asset('dashboard.png') }}" alt="Dashboard" class="w-6 h-6 mr-2">
+                    Dashboard
                 </a>
-                <hr>
-                <ul class="nav nav-pills flex-column mb-auto">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link active" aria-current="page">
-                            <svg class="bi pe-none me-2" width="16" height="16">
-                                <use xlink:href="#home"></use>
-                            </svg>
-                            Home
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16">
-                                <use xlink:href="#speedometer2"></use>
-                            </svg>
-                            Dashboard
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16">
-                                <use xlink:href="#table"></use>
-                            </svg>
-                            Settings
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16">
-                                <use xlink:href="#grid"></use>
-                            </svg>
-                            History
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link text-white">
-                            <svg class="bi pe-none me-2" width="16" height="16">
-                                <use xlink:href="#people-circle"></use>
-                            </svg>
-                            Profile
-                        </a>
-                    </li>
-                </ul>
-                <hr>
-                <div class="dropdown">
-                    <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="" width="32" height="32"
-                            class="rounded-circle me-2">
-                        <strong>mdo</strong>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                        <li><a class="dropdown-item" href="#">New project...</a></li>
-                        <li><a class="dropdown-item" href="#">Settings</a></li>
-                        <li><a class="dropdown-item" href="#">Profile</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Sign out</a></li>
-                    </ul>
+                <a href="/currency-rate" class="flex items-center px-4 py-2 text-gray-600 hover:text-indigo-600 hover:bg-gray-100 py-8">
+                    <img src="{{ asset('rate.png') }}" alt="Currency Rate" class="w-6 h-6 mr-2">
+                    Currency Rate
+                </a>
+                <a href="/transactions" class="flex items-center px-4 py-2 text-gray-600 hover:text-indigo-600 hover:bg-gray-100 py-8">
+                    <img src="{{ asset('transaction.png') }}" alt="Transactions" class="w-6 h-6 mr-2">
+                    Transactions
+                </a>
+                <a href="/notification" class="flex items-center px-4 py-2 text-gray-600 hover:text-indigo-600 hover:bg-gray-100 py-8">
+                    <img src="{{ asset('notify.png') }}" alt="Notification" class="w-6 h-6 mr-2">
+                    Notification
+                </a>
+                <a href="/support" class="flex items-center px-4 py-2 text-gray-600 hover:text-indigo-600 hover:bg-gray-100 py-8">
+                    <img src="{{ asset('support.png') }}" alt="Support" class="w-6 h-6 mr-2">
+                    Support
+                </a>
+                <a href="/" class="flex items-center px-4 py-2 text-gray-600 hover:text-indigo-600 hover:bg-gray-100 py-8">
+                    <img src="{{ asset('logout.png') }}" alt="Log Out" class="w-6 h-6 mr-2">
+                    Log Out
+                </a>
+            </nav>
+        </div>
+        <!-- Main Content -->
+        <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+            <div class="px-6 py-8">
+                <div class="flex items-center justify-between">
+                    <h1 class="text-2xl font-semibold text-gray-800">Welcome Timothy</h1>
+                    <p class="text-gray-600">Make your seamless transfer today!</p>
+                </div>
+                <div class="mt-6 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+                    <!-- Quick Transfer -->
+                    <div class="p-6 bg-white rounded-lg shadow-lg">
+                        <div class="flex items-center justify-between">
+                            <h2 class="text-xl font-semibold text-gray-800">Make a quick transfer</h2>
+                            <img src="{{ asset('transfer.png') }}" alt="Quick Transfer" class="w-8 h-8">
+                        </div>
+                        <p class="mt-4 text-gray-600">Send money to all individuals across the continent.</p>
+                        <div class="mt-6">
+                            <button class="px-4 py-2 text-white bg-indigo-600 rounded-full hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200">
+                                Transfer money
+                            </button>
+                        </div>
+                    </div>
+                    <!-- Custom Request -->
+                    <div class="p-6 bg-white rounded-lg shadow-lg">
+                        <div class="flex items-center justify-between">
+                            <h2 class="text-xl font-semibold text-gray-800">Make a custom request</h2>
+                            <img src="{{ asset('customise.png') }}" alt="Custom Request" class="w-8 h-8">
+                        </div>
+                        <p class="mt-4 text-gray-600">We offer you the opportunity to make a currency request.</p>
+                        <div class="mt-6">
+                            <button class="px-4 py-2 text-white bg-indigo-600 rounded-full hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200">
+                                Customized request
+                            </button>
+                        </div>
+                    </div>
+                    <!-- View Rate -->
+                    <div class="p-6 bg-white rounded-lg shadow-lg">
+                        <div class="flex items-center justify-between">
+                            <h2 class="text-xl font-semibold text-gray-800">View Currency Rates</h2>
+                            <img src="{{ asset('bankrate.png') }}" alt="Currency Rates" class="w-8 h-8">
+                        </div>
+                        <p class="mt-4 text-gray-600">Get up-to-date currency rates for all your transactions.</p>
+                        <div class="mt-6">
+                            <button class="px-4 py-2 text-white bg-indigo-600 rounded-full hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-indigo-200">
+                                View rates
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- Recent Transactions -->
+                <div class="mt-8">
+                    <h2 class="text-xl font-semibold text-gray-800">Recent Transactions</h2>
+                    <div class="mt-4 bg-white rounded-lg shadow-lg">
+                        <div class="p-4">
+                            <!-- Transaction 1 -->
+                            <div class="flex justify-between mb-4">
+                                <div>
+                                    <h3 class="text-lg font-semibold text-gray-800">12 September</h3>
+                                    <p class="text-gray-600">You paid 10,000 NGN to Jane Doe</p>
+                                </div>
+                                <p class="text-green-600">+10,000 NGN</p>
+                            </div>
+                            <!-- Transaction 2 -->
+                            <div class="flex justify-between mb-4">
+                                <div>
+                                    <h3 class="text-lg font-semibold text-gray-800">12 September</h3>
+                                    <p class="text-gray-600">You paid 10,000 NGN to Jane Doe</p>
+                                </div>
+                                <p class="text-green-600">+10,000 NGN</p>
+                            </div>
+                            <!-- Transaction 3 -->
+                            <div class="flex justify-between mb-4">
+                                <div>
+                                    <h3 class="text-lg font-semibold text-gray-800">12 September</h3>
+                                    <p class="text-gray-600">You paid 10,000 NGN to Jane Doe</p>
+                                </div>
+                                <p class="text-green-600">+10,000 NGN</p>
+                            </div>
+                            <!-- Transaction 4 -->
+                            <div class="flex justify-between mb-4">
+                                <div>
+                                    <h3 class="text-lg font-semibold text-gray-800">12 September</h3>
+                                    <p class="text-gray-600">You paid 10,000 NGN to Jane Doe</p>
+                                </div>
+                                <p class="text-green-600">+10,000 NGN</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <!-- Main content -->
-            <main class="col-md-9 ml-md-auto px-4">
-                <!-- Transaction History -->
-                <div class="row">
-                    <div class="col-md-12">
-                        <!-- Transaction history table -->
-                    </div>
-                </div>
-
-                <!-- Account Summary and Graph -->
-                <div class="row">
-                    <div class="col-md-6">
-                        <h2>Account Summary</h2>
-                        <p><strong>Account Balance:</strong> $1,200</p>
-                        <p><strong>Available Funds:</strong> $800</p>
-                        <p><strong>Pending Transactions:</strong> 2</p>
-                    </div>
-                    <div class="col-md-6">
-                        <h2>Graphs and Charts</h2>
-                        <div class="text-center">
-                            <h4>Placeholder Graph</h4>
-                            <p>This is where the actual graph will go.</p>
-                        </div>
-                        <div class="d-flex align-items-end">
-                            <div class="flex-shrink-0">
-                                <img src="#" alt="" width="300">
-                            </div>
-                            <div class="flex-grow-1 ms-3">
-                                <h5 class="mt-0">Media heading</h5>
-                                <p>This is some content from a media component</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Quick Actions -->
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2>Quick Actions</h2>
-                        <a href="#" class="btn btn-primary">Send Money</a>
-                        <a href="#" class="btn btn-success">Receive Money</a>
-                        <a href="#" class="btn btn-info">Request Funds</a>
-                        <a href="#" class="btn btn-secondary">Make Payment</a>
-                    </div>
-                </div>
-            </main>
-        </div>
+        </main>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 
 </html>
