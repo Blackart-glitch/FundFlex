93329a5e5ebfbf750a8f674def2f40da1efb27e0
Daniel Boye [Author] <boyelanrewaju@gmail.com>
2023-09-23 18:02:43 +0100

    update to design and content

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -109,7 +109,7 @@
             </div>
 
             <div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-                <p>&copy; 2023-2024 Your E-Wallet, Inc. All rights reserved.</p>
+                <p>&copy; 2023-2024 FundFlex, Inc. All rights reserved.</p>
                 <ul class="list-unstyled d-flex">
                     <li class="ms-3"><a class="link-dark" href="https://twitter.com/your_e_wallet"><svg class="bi"
                                 width="24" height="24">
